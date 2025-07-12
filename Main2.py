fb65179ab0565e96f102653b62689d9e7ed4c6ba
GholamrezaMaleki <163832349+GholamrezaMaleki@users.noreply.github.com>
2025-07-12 20:53:34 +0330

    secondCommit_detectingHands_&&_settingTimer

--- Main2.py
+++ Main2.py
@@ -9,7 +9,12 @@ hands = mpHands.Hands()
 mpDraw = mp.solutions.drawing_utils
 pTime = 0
 cTime = 0
+clock = 0
+winner = ""
+p1_move = p2_move = None
+gameText = ""
 
+success = True
 while True:
     ret, img = cap.read()
     imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
@@ -17,13 +22,30 @@ while True:
     if results.multi_hand_landmarks:
         for handLms in results.multi_hand_landmarks:
             mpDraw.draw_landmarks(img, handLms, mpHands.HAND_CONNECTIONS)
-            print(handLms)
-    cTime = time.time()
-    fps = 1 / (cTime - pTime)
-    pTime = cTime
-    cv2.putText(img, str(int(fps)), (10, 70), cv2.FONT_HERSHEY_PLAIN, 2, (0, 0, 255), 2)
+
+    if 0 <= clock < 20:
+        success = True
+        gameText = "Are You Ready?"
+    elif clock < 30:
+        gameText = "3..."
+    elif clock < 40:
+        gameText = "2..."
+    elif clock < 50:
+        gameText = "1..."
+    elif clock < 60:
+        gameText = "Go!!"
+    elif clock == 60:
+        hls = results.multi_hand_landmarks
+        if hls :
+            if len(hls)==2:
+                print("two hands detected")
+            elif len(hls)==1:
+                print("one hand detected")
     if not ret:
         break
+    cv2.putText(img, f"clock : {clock}", (50, 50), cv2.FONT_HERSHEY_PLAIN, 1, (255, 0, 0), 2, cv2.LINE_AA)
+    cv2.putText(img, gameText, (50, 80), cv2.FONT_HERSHEY_PLAIN, 1, (0, 0, 255), 2, cv2.LINE_AA)
+    clock = (clock + 0.5) % 100
     cv2.imshow('frame', img)
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
